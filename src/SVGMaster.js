c4754e160ce9d54ec8ba67a7802698ae2ac7e1de
Pierluigi Pesenti <pierluigi.pesenti@appway.com>
2016-06-28 08:04:14 +0200

    added eslint configuration file, autoformatted code, removed two unused constants

--- src/SVGMaster.js
+++ src/SVGMaster.js
@@ -1,40 +1,38 @@
 (function() {
-	var SVGMaster=(function() {
+	var SVGMaster = (function() {
 		'use strict';
 
-		var NS_SVG='http://www.w3.org/2000/svg';
-		var NS_XLINK='http://www.w3.org/1999/xlink';
+		var NS_SVG = 'http://www.w3.org/2000/svg';
+		var NS_XLINK = 'http://www.w3.org/1999/xlink';
 
-		var ATTRIBUTE_CLASS='class';
-		var ATTRIBUTE_PARSED='data-svgmaster-parsed';
-		var ATTRIBUTE_HREF='href';
-		var ATTRIBUTE_ID='id';
-		var REGEXP_ICON_ID=/icon-([a-zA-Z0-9]+(-[a-zA-Z0-9]+)*)/;
-		var REGEXP_TEMPLATE=/{{([^}}]+)}}/g;
+		var ATTRIBUTE_CLASS = 'class';
+		var ATTRIBUTE_PARSED = 'data-svgmaster-parsed';
+		var ATTRIBUTE_HREF = 'href';
+		var ATTRIBUTE_ID = 'id';
+		var REGEXP_ICON_ID = /icon-([a-zA-Z0-9]+(-[a-zA-Z0-9]+)*)/;
+		var REGEXP_TEMPLATE = /{{([^}}]+)}}/g;
 
-		var TEMPLATE_DEFAULT_LIBRARY_LINK='link[rel="{{attributes}}"]';
-		var TEMPLATE_SVG_BASE64_CONTENT='url(data:image/svg+xml;base64,{{content}})';
-		var TEMPLATE_SVG_BASE64='<svg xmlns="'+NS_SVG+'"  xmlns:xlink="'+NS_XLINK+'" width="10" height="10">{{style}}{{symbol}}<use xlink:href="#iconSymbol-{{iconID}}" width="100%" height="100%" /></svg>';
-		var TEMPLATE_SVG_INLINE='<svg class="{{class}}"><use xlink:href="#iconSymbol-{{iconID}}" width="100%" height="100%" /></svg>';
-		var CLASS_SHOWCASE='SVGMasterShowcase';
-		var TEMPLATE_SHOWCASE='<div class="SVGMasterWrapper"><div class="'+CLASS_SHOWCASE+'"><header>{{headerTitle}}</header><ul class="SVGMasterShowcaseList">{{contents}}</ul><footer></footer></div></div>';
-		var TEMPLATE_SHOWCASE_ITEM='<li class="SVGMasterShowcaseItem"><i class="icon icon-{{id}}"></i><span title="{{title}}">{{title}}</span></li>';
-		var STRING_DEFAULT_LIBRARY_REL='prefetch svgmaster';
-		var STRING_AJAX_DATATYPE='text';
-		var STRING_ICON_SYMBOL_PREFIX='iconSymbol-';
-		var TAG_SYMBOL='symbol';
-		var TAG_USE='use';
-		var TAG_STYLE='style';
-		var TAG_TITLE='title';
-		var TAG_PARAGRAPH='p';
-		var EVENT_CLICK='click';
+		var TEMPLATE_DEFAULT_LIBRARY_LINK = 'link[rel="{{attributes}}"]';
+		var TEMPLATE_SVG_BASE64_CONTENT = 'url(data:image/svg+xml;base64,{{content}})';
+		var TEMPLATE_SVG_BASE64 = '<svg xmlns="' + NS_SVG + '"  xmlns:xlink="' + NS_XLINK + '" width="10" height="10">{{style}}{{symbol}}<use xlink:href="#iconSymbol-{{iconID}}" width="100%" height="100%" /></svg>';
+		var TEMPLATE_SVG_INLINE = '<svg class="{{class}}"><use xlink:href="#iconSymbol-{{iconID}}" width="100%" height="100%" /></svg>';
+		var CLASS_SHOWCASE = 'SVGMasterShowcase';
+		var TEMPLATE_SHOWCASE = '<div class="SVGMasterWrapper"><div class="' + CLASS_SHOWCASE + '"><header>{{headerTitle}}</header><ul class="SVGMasterShowcaseList">{{contents}}</ul><footer></footer></div></div>';
+		var TEMPLATE_SHOWCASE_ITEM = '<li class="SVGMasterShowcaseItem"><i class="icon icon-{{id}}"></i><span title="{{title}}">{{title}}</span></li>';
+		var STRING_DEFAULT_LIBRARY_REL = 'prefetch svgmaster';
+		var STRING_AJAX_DATATYPE = 'text';
+		var STRING_ICON_SYMBOL_PREFIX = 'iconSymbol-';
+		var TAG_SYMBOL = 'symbol';
+		var TAG_USE = 'use';
+		var TAG_STYLE = 'style';
+		var TAG_TITLE = 'title';
 
-		var SELECTOR_DEFAULT_ICON='i.icon';
-		var SELECTOR_DEFAULT_BG='.iconBg';
-		var SELECTOR_SHOWCASE_WRAPPER='.SVGMasterWrapper';
+		var SELECTOR_DEFAULT_ICON = 'i.icon';
+		var SELECTOR_DEFAULT_BG = '.iconBg';
+		var SELECTOR_SHOWCASE_WRAPPER = '.SVGMasterWrapper';
 
-		var iconsSelector=SELECTOR_DEFAULT_ICON;
-		var bgsSelector=SELECTOR_DEFAULT_BG;
+		var iconsSelector = SELECTOR_DEFAULT_ICON;
+		var bgsSelector = SELECTOR_DEFAULT_BG;
 		var libraryURL;
 		var library;
 		var head;
@@ -54,7 +52,7 @@
 		 * @param {String} selector The selector string
 		 */
 		function setIconSelector(selector) {
-			iconsSelector=selector;
+			iconsSelector = selector;
 		}
 		/**
 		 * Gets the icons selector pattern to match in jQuery selector format
@@ -68,7 +66,7 @@
 		 * @param {String} selector The selector string
 		 */
 		function setBackgroundSelector(selector) {
-			bgsSelector=selector;
+			bgsSelector = selector;
 		}
 		/**
 		 * Gets the backgrounds selector pattern to match in jQuery selector format
@@ -100,9 +98,9 @@
 		 */
 		function getIconElements(target) {
 			// target parameter is checked here instead than in the update() method so that the function can be used publicly
-			body=$('body');
-			var target=target || body;
-			return target.find(iconsSelector).not('['+ATTRIBUTE_PARSED+']');
+			body = $('body');
+			target = target || body;
+			return target.find(iconsSelector).not('[' + ATTRIBUTE_PARSED + ']');
 		}
 		/**
 		 * Retrieves the matching background elements, without performing any operation
@@ -111,9 +109,9 @@
 		 */
 		function getBackgroundElements(target) {
 			// target parameter is checked here instead than in the update() method so that the function can be used publicly
-			body=$('body');
-			var target=target || body;
-			return target.find(bgsSelector).not('['+ATTRIBUTE_PARSED+']')
+			body = $('body');
+			target = target || body;
+			return target.find(bgsSelector).not('[' + ATTRIBUTE_PARSED + ']')
 		}
 		/**
 		 * Replace elements matching IconsSelector selector with the svg item while keeping the classes
@@ -121,9 +119,12 @@
 		 */
 		function replaceIcons(target) {
 			getIconElements(target).each(function() {
-				var currClass=$(this).attr(ATTRIBUTE_CLASS);
-				var iconID=getIconID(currClass);
-				var replacement=$(template(TEMPLATE_SVG_INLINE, {class:currClass, iconID:iconID}));
+				var currClass = $(this).attr(ATTRIBUTE_CLASS);
+				var iconID = getIconID(currClass);
+				var replacement = $(template(TEMPLATE_SVG_INLINE, {
+					class: currClass,
+					iconID: iconID
+				}));
 				replacement.attr(ATTRIBUTE_PARSED, true);
 				$(this).replaceWith(replacement);
 			});
@@ -134,20 +135,26 @@
 		 */
 		function replaceBackgrounds(target) {
 			getBackgroundElements(target).each(function() {
-				var currClass=$(this).attr(ATTRIBUTE_CLASS);
-				var iconID=getIconID(currClass);
-				library=$('#library');
-				var matches=library.find('#'+STRING_ICON_SYMBOL_PREFIX+iconID);
-				var symbolSVG=outerHtml(matches);
-				var matchesStyle=library.find(TAG_STYLE);
-				var styleSVG='';
-				if(matchesStyle.length>0) {
-					styleSVG=outerHtml(matchesStyle);
+				var currClass = $(this).attr(ATTRIBUTE_CLASS);
+				var iconID = getIconID(currClass);
+				library = $('#library');
+				var matches = library.find('#' + STRING_ICON_SYMBOL_PREFIX + iconID);
+				var symbolSVG = outerHtml(matches);
+				var matchesStyle = library.find(TAG_STYLE);
+				var styleSVG = '';
+				if (matchesStyle.length > 0) {
+					styleSVG = outerHtml(matchesStyle);
 				}
 
-				var outSvg=template(TEMPLATE_SVG_BASE64, {style:styleSVG, symbol:symbolSVG, iconID:iconID});
+				var outSvg = template(TEMPLATE_SVG_BASE64, {
+					style: styleSVG,
+					symbol: symbolSVG,
+					iconID: iconID
+				});
 				$(this).css({
-					'background-image': template(TEMPLATE_SVG_BASE64_CONTENT, {content:btoa(outSvg)})
+					'background-image': template(TEMPLATE_SVG_BASE64_CONTENT, {
+						content: btoa(outSvg)
+					})
 				});
 				$(this).attr(ATTRIBUTE_PARSED, true);
 			});
@@ -157,7 +164,9 @@
 		 * @return {String} The url of the default library
 		 */
 		function getDefaultLibraryURL() {
-			var link=$(template(TEMPLATE_DEFAULT_LIBRARY_LINK, {attributes:STRING_DEFAULT_LIBRARY_REL}));
+			var link = $(template(TEMPLATE_DEFAULT_LIBRARY_LINK, {
+				attributes: STRING_DEFAULT_LIBRARY_REL
+			}));
 			return link.attr(ATTRIBUTE_HREF);
 		}
 		/**
@@ -169,12 +178,15 @@
 			// or eventually use in page svg libraries instead of external resources but inlining would be discouraged 
 			// to enforce caching.
 			url = url || getDefaultLibraryURL();
-			libraryURL=url;
-			if(url && typeof $.ajax !== 'undefined') {
-				$.ajax({url:libraryURL, dataType:STRING_AJAX_DATATYPE}).done(function(res) {
-					head=$('head');
+			libraryURL = url;
+			if (url && typeof $.ajax !== 'undefined') {
+				$.ajax({
+					url: libraryURL,
+					dataType: STRING_AJAX_DATATYPE
+				}).done(function(res) {
+					head = $('head');
 					head.append(res);
-					library=$('#library');
+					library = $('#library');
 					library.find(TAG_USE).remove();
 					head.append(library.find(TAG_STYLE).clone());
 					update();
@@ -185,11 +197,14 @@
 		 * Opens the library showcase dialog
 		 */
 		function showcase() {
-			var showcaseContent=template(TEMPLATE_SHOWCASE, {contents:getShowcaseContents(), headerTitle: libraryURL});
-			body=$('body');
+			var showcaseContent = template(TEMPLATE_SHOWCASE, {
+				contents: getShowcaseContents(),
+				headerTitle: libraryURL
+			});
+			body = $('body');
 			body.append($(showcaseContent));
 			// replace only icons on specific target. Backgrounds are not used in the showcase
-			replaceIcons($('.'+CLASS_SHOWCASE));
+			replaceIcons($('.' + CLASS_SHOWCASE));
 		}
 		/**
 		 * Hides the showcase dialog
@@ -202,13 +217,16 @@
 		 * @return {String} string representation of the markup to be appended to the DOM
 		 */
 		function getShowcaseContents() {
-			var out='';
-			library=$('#library');
+			var out = '';
+			library = $('#library');
 			library.find(TAG_SYMBOL).each(function() {
-				var t=$(this);
-				var title=t.find(TAG_TITLE).text();
-				var id=t.attr(ATTRIBUTE_ID).split(STRING_ICON_SYMBOL_PREFIX).join('');
-				out+=template(TEMPLATE_SHOWCASE_ITEM, {title:title, id:id});
+				var t = $(this);
+				var title = t.find(TAG_TITLE).text();
+				var id = t.attr(ATTRIBUTE_ID).split(STRING_ICON_SYMBOL_PREFIX).join('');
+				out += template(TEMPLATE_SHOWCASE_ITEM, {
+					title: title,
+					id: id
+				});
 			});
 			return out;
 		}
@@ -219,14 +237,17 @@
 		 * @return {String} The final string assembled
 		 */
 		function template(templateStr, dataObj) {
-			var out=templateStr;
-			var regexp=REGEXP_TEMPLATE;
+			var out = templateStr;
+			var regexp = REGEXP_TEMPLATE;
 			var result;
-			while(result!==null) {
-				result=regexp.exec(templateStr);
-				if(result!==null) {
-					var matchObj={match:result[0], variable:result[1]};
-					out=out.replace(matchObj.match, dataObj[matchObj.variable]);
+			while (result !== null) {
+				result = regexp.exec(templateStr);
+				if (result !== null) {
+					var matchObj = {
+						match: result[0],
+						variable: result[1]
+					};
+					out = out.replace(matchObj.match, dataObj[matchObj.variable]);
 				}
 			}
 			return out;
@@ -242,25 +263,25 @@
 
 		// PUBLIC API
 		return {
-			getIconElements:getIconElements,
-			getBackgroundElements:getBackgroundElements,
-			replaceIcons:replaceIcons,
-			replaceBackgrounds:replaceBackgrounds,
-			update:update,
-			loadLibrary:loadLibrary,
-			setIconSelector:setIconSelector,
-			getIconSelector:getIconSelector,
-			setBackgroundSelector:setBackgroundSelector,
-			getBackgroundSelector:getBackgroundSelector,
-			showcase:showcase,
-			hideShowcase:hideShowcase,
-			getDefaultLibraryURL:getDefaultLibraryURL
+			getIconElements: getIconElements,
+			getBackgroundElements: getBackgroundElements,
+			replaceIcons: replaceIcons,
+			replaceBackgrounds: replaceBackgrounds,
+			update: update,
+			loadLibrary: loadLibrary,
+			setIconSelector: setIconSelector,
+			getIconSelector: getIconSelector,
+			setBackgroundSelector: setBackgroundSelector,
+			getBackgroundSelector: getBackgroundSelector,
+			showcase: showcase,
+			hideShowcase: hideShowcase,
+			getDefaultLibraryURL: getDefaultLibraryURL
 		};
 	})();
 
-	if(typeof module!=='undefined' && typeof module.exports!=='undefined') {
-		module.exports=SVGMaster;
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+		module.exports = SVGMaster;
 	} else {
-		window.SVGMaster=SVGMaster;
+		window.SVGMaster = SVGMaster;
 	}
 })();
