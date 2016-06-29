98ccfcee6a6bf0af07838889c91eb2651977081d
Pierluigi Pesenti <pierluigi.pesenti@appway.com>
2016-06-29 18:11:51 +0200

    added svgstore at the gruntfile and raw svg elements to build libraries

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,12 +17,31 @@ module.exports = function(grunt) {
           'dist/<%= pkg.name %>.showcase.min.css': ['src/<%= pkg.name %>.showcase.css']
         }
       }
+    },
+    svgstore: {
+      default: {
+        files: {
+          'dist/libraries/icons/highschool.svg': ['libraries/icons/highschool/*.svg'],
+          'dist/libraries/background/freepik.svg': ['libraries/backgrounds/freepik/*.svg']
+        }
+      },
+      options: {
+        prefix : 'iconSymbol-', // This will prefix each ID
+        svg: { // will add and overide the the default xmlns="http://www.w3.org/2000/svg" attribute to the resulting SVG
+          viewBox : '0 0 470 470',
+          xmlns: 'http://www.w3.org/2000/svg',
+          id:'library',
+          'xmlns:xlink':'http://www.w3.org/1999/xlink'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-svgstore');
 
   grunt.registerTask('default', ['uglify', 'cssmin']);
+  grunt.registerTask('libraries', ['svgstore']);
 
 };
